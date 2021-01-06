57463b9829112a7c2f2ea01c33f005238d984242
Jayram <Jayram1234patil@gmail.com>
2021-01-06 23:34:33 +0530

    Finished ASCII Assignment

--- ASCII Values From Character/script.js
+++ ASCII Values From Character/script.js
@@ -1,7 +1,9 @@
 var char = document.body.addEventListener('keypress', function(e){
 
-    var p = document.getElementById("ascii");
+    var p1 = document.getElementById("asciichar");
+    var p2 = document.getElementById("asciivalue");
 
-    p.innerHTML = e.key + " " + e.keyCode;
+    p1.innerHTML = e.key  
+    p2.innerHTML = e.keyCode;
 
 })
