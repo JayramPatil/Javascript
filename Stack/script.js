a343ddd5bd23cfc2817560e248a1be3a8e360264
Jayram <Jayram1234patil@gmail.com>
2021-01-12 22:06:48 +0530

    Final UI fIXES

--- Stack/script.js
+++ Stack/script.js
@@ -3,6 +3,9 @@ var i = 2
 var push = document.getElementById('push');
 var pop = document.getElementById('pop');
 var c = document.getElementById('secDiv');
+var Top = document.getElementById('top');
+var topN = document.getElementById('topN');
+
 
 push.addEventListener('click', function(){
 
@@ -16,16 +19,24 @@ push.addEventListener('click', function(){
         d.id = mar;
         d.style.marginBottom =  mar + "px";
         d.textContent = i++;
+        Top.style.marginBottom = (mar+60) + "px";
+        topN.style.marginBottom = (mar+70) + "px";
 
         mar += 60;
 
         c.appendChild(d);
     }
     else if(mar == 10){
-
+        topN.style.marginBottom = (mar+70) + "px";
+        Top.style.marginBottom = (mar+60) + "px";
         elem.style.backgroundColor = "white";
         elem.textContent = "1";
         mar+=60
+    }
+    else
+    {
+        alert("Stack Is Full !!! Cant Insert New Element Because Stack Is Already Full.");
+
     }
 })
 
@@ -39,14 +50,23 @@ pop.addEventListener('click', function(){
 
         var d = document.getElementById(mar.toString());
         d.textContent = "";
+        Top.style.marginBottom = mar + "px";
+        topN.style.marginBottom = (mar+10) + "px";
         i--;
         c.removeChild(d);
+        console.log(d.style.marginBottom);
 
     }
-    else{
-
+    else if(mar == 10){
         elem.style.backgroundColor = "lightcoral";
         elem.textContent = "";
         mar = 10;
+        Top.style.marginBottom = mar + "px";
+        topN.style.marginBottom = (mar+10) + "px";
+    }
+    else{
+        alert("Stack Is Empty !!! Cant POP Any Element Because Stack Is Already Empty.");
+        mar = 10;
+
     }
 })
