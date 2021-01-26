cdfadb0c45a36990a84d14d294ac858ac81645ce
Jayram <Jayram1234patil@gmail.com>
2021-01-27 00:26:27 +0530

    Added Operators Code In Javascript

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,42 +1,67 @@
-let buttons = document.querySelector('.keyboard')
-let display = document.getElementById('dis')
+const calc = document.querySelector('.calculator');
+let buttons = document.querySelector('.keyboard');
+let display = document.getElementById('dis');
 
 
 buttons.addEventListener('click',function(e){
-    let i = e.target;
-    console.log(i.textContent);
-    if(display.textContent == '0'){
-        display.textContent = i.textContent;
+    const key = e.target;
+    const action =key.dataset.action;
+    if(e.target.matches('button'))
+    {
+        console.log(action);
+        if(action === 'add' || action === 'substract' || action === 'multiply' || action === 'divide')
+        {
+            Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
+            Display(key.textContent);
+            key.classList.add('pressed');
+            calc.dataset.operator = action;
+        }
+        else if(action === 'clear')
+        {
+            Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
 
-    }
-    else if(i.textContent == 'AC')
-    {
-        display.textContent = '';
-    }
-    else if(i.textContent == '=')
-    {
-        Operation('*');
-    }
-    else{
-        display.textContent = display.textContent + i.textContent;
+            display.textContent = '';
+        }
+        else if(action === 'dot')
+        {
+
+        }
+        else if(action === 'equalto')
+        {
+            Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
+            Display(key.textContent);
+            Operation(calc.dataset.operator);
+        }
+        else{
+            Display(key.textContent);
+        }
+        
     }
 })
-
+function Display(value)
+{
+    if(display.textContent == '0'){
+        display.textContent = value;
+    }
+    else{
+        display.textContent = display.textContent + value;
+    }
+}
 function Operation(operator){
-    if(operator == '+'){
+    if(operator == 'add'){
         let Arr = display.textContent.split('+');
         display.textContent = parseInt(Arr[0]) + parseInt(Arr[1]);
     }
-    else if(operator == '-'){
-        let Arr = display.textContent.split('+');
-        display.textContent = parseInt(Arr[0]) + parseInt(Arr[1]);
+    else if(operator == 'substract'){
+        let Arr = display.textContent.split('-');
+        display.textContent = parseInt(Arr[0]) - parseInt(Arr[1]);
     }
-    else if(operator == '*'){
-        let Arr = display.textContent.split('&times;');
+    else if(operator == 'multiply'){
+        let Arr = display.textContent.split('NaN');
         display.textContent = parseInt(Arr[0]) * parseInt(Arr[1]);
     }
-    else if(operator == '+'){
-        let Arr = display.textContent.split('+');
-        display.textContent = parseInt(Arr[0]) + parseInt(Arr[1]);
+    else if(operator == 'divide'){
+        let Arr = display.textContent.split('%');
+        display.textContent = parseInt(Arr[0]) % parseInt(Arr[1]);
     }
 }
