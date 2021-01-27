baa320e65fc95dc1f094847913178f604c917f5d
Jayram <Jayram1234patil@gmail.com>
2021-01-28 00:05:26 +0530

    Added Basic Functions In Javascript

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,41 +1,80 @@
 const calc = document.querySelector('.calculator');
-let buttons = document.querySelector('.keyboard');
-let display = document.getElementById('dis');
-
+const buttons = document.querySelector('.keyboard');
+const display = document.getElementById('dis');
+let OpCount = 0;
+let LastKey = null;
 
 buttons.addEventListener('click',function(e){
     const key = e.target;
     const action =key.dataset.action;
     if(e.target.matches('button'))
     {
-        console.log(action);
+        
         if(action === 'add' || action === 'substract' || action === 'multiply' || action === 'divide')
         {
-            Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
-            Display(key.textContent);
-            key.classList.add('pressed');
-            calc.dataset.operator = action;
+            if(LastKey != '+' && LastKey != '-' && LastKey != '*' && LastKey != '/')
+            {
+                Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
+                key.classList.add('pressed');
+                OpCount++;
+                if(OpCount == 1)
+                {
+                    calc.dataset.firstNumber = display.textContent;
+                    calc.dataset.operator = action;
+                }
+                else
+                {
+                    calc.dataset.firstNumber = preCalculate(calc.dataset.firstNumber,calc.dataset.operator); 
+                    console.log(calc.dataset.firstNumber);
+                    calc.dataset.secondNumber = 0;
+                    calc.dataset.operator = action;
+                }
+                (display.textContent == '0')?Display('0'+ key.textContent):Display(key.textContent);
+
+                LastKey = key.textContent;
+            }
         }
         else if(action === 'clear')
         {
             Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
 
-            display.textContent = '';
+            display.textContent = '0';
+
+            LastKey = null;
         }
         else if(action === 'dot')
         {
-
+            if(LastKey != '+' && LastKey != '-' && LastKey != '*' && LastKey != '/')
+            {
+                if(display.textContent == '0')
+                {
+                    Display('0.');
+                }
+                else
+                {
+                    Display('.');
+                }
+                LastKey = key.textContent;
+            }
         }
         else if(action === 'equalto')
         {
-            Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
-            Display(key.textContent);
-            Operation(calc.dataset.operator);
+            if(LastKey != '+' && LastKey != '-' && LastKey != '*' && LastKey != '/')
+            {
+                OpCount = 0;
+                Array.from(key.parentNode.children).forEach(k => k.classList.remove('pressed'));
+                Display(key.textContent);
+                calc.dataset.firstNumber = Operation(calc.dataset.operator,calc.dataset.firstNumber);
+                display.textContent = calc.dataset.firstNumber;
+
+                LastKey = key.textContent;
+            }
         }
         else{
             Display(key.textContent);
+            LastKey = key.textContent;
+
         }
-        
     }
 })
 function Display(value)
@@ -47,21 +86,39 @@ function Display(value)
         display.textContent = display.textContent + value;
     }
 }
-function Operation(operator){
+function Operation(operator,first){
     if(operator == 'add'){
-        let Arr = display.textContent.split('+');
-        display.textContent = parseInt(Arr[0]) + parseInt(Arr[1]);
+        let Arr = display.textContent.split('+').pop();
+        return parseInt(first) + parseInt(Arr[0]);
     }
     else if(operator == 'substract'){
-        let Arr = display.textContent.split('-');
-        display.textContent = parseInt(Arr[0]) - parseInt(Arr[1]);
+        let Arr = display.textContent.split('-').pop();
+        return parseInt(first) - parseInt(Arr[0]);
     }
     else if(operator == 'multiply'){
-        let Arr = display.textContent.split('NaN');
-        display.textContent = parseInt(Arr[0]) * parseInt(Arr[1]);
+        let Arr = display.textContent.split('*').pop();
+        return parseInt(first) * parseInt(Arr[0]);
     }
     else if(operator == 'divide'){
-        let Arr = display.textContent.split('%');
-        display.textContent = parseInt(Arr[0]) % parseInt(Arr[1]);
+        let Arr = display.textContent.split('/').pop();
+        return parseInt(first) / parseInt(Arr[0]);
+    }
+}
+function preCalculate(first,operator){
+    if(operator == 'add'){
+        let Arr = display.textContent.split('+').pop();
+        return parseInt(first) + parseInt(Arr[0]);
+    }
+    else if(operator == 'substract'){
+        let Arr = display.textContent.split('-').pop();
+        return parseInt(first) - parseInt(Arr[0]);
+    }
+    else if(operator == 'multiply'){
+        let Arr = display.textContent.split('*').pop();
+        return parseInt(first) * parseInt(Arr[0]);
+    }
+    else if(operator == 'divide'){
+        let Arr = display.textContent.split('/').pop();
+        return parseInt(first) / parseInt(Arr[0]);
     }
 }
