ba9aa3f9d5c1209379e31482e5e07d3d4c2c56af
Jayram <Jayram1234patil@gmail.com>
2021-02-18 20:29:48 +0530

    Amicable Number Program

---
+++ Basic Practice Programs/Amicable.js
@@ -0,0 +1,33 @@
+let start = 11,end = 2000;
+
+while(start <= end)
+{
+    let sum1 = 0, sum2 = 0, i = 0;
+
+    for(i = 1; i<=(start/2); i++)
+    {
+        if(start%i == 0)
+        {
+            sum1 += i;
+        }
+    }
+
+    if(start != sum1)
+    {
+        for(i = 1; i<=(sum1/2); i++)
+        {
+            if(sum1%i == 0)
+            {
+                sum2 += i;
+            }
+        }
+    }
+
+    if(sum2 == start)
+    {
+        document.write("<br>Amicable Number => " + start);
+    }
+    
+    sum1 = sum2 = 0;
+    start++;
+}
