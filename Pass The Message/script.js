91a51638a743309b9342b0ea360598ae77b885fb
Jayram <Jayram1234patil@gmail.com>
2021-03-06 22:45:15 +0530

    Added JS Code

---
+++ Pass The Message/script.js
@@ -0,0 +1,21 @@
+let textBox = document.getElementById('text')
+let btn = document.getElementById('btn')
+let lastMsg = document.getElementById('lastmsg')
+let warning = document.getElementById('warning')
+
+btn.addEventListener('click',function(){
+    console.log(textBox.value)
+    if(textBox.value != "")
+    {
+        lastMsg.textContent = textBox.value
+        textBox.value = ""
+    }
+    else
+    {
+        warning.style.display = "block"
+        setTimeout(() => {
+            warning.style.display = "none"
+        }, 2000);
+        
+    }
+})
