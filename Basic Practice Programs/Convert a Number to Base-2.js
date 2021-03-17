509d96c82e0738691c2250ccf31f2a5936fdd944
Jayram <Jayram1234patil@gmail.com>
2021-03-17 23:22:46 +0530

    Added Another Way

--- Basic Practice Programs/Convert a Number to Base-2.js
+++ Basic Practice Programs/Convert a Number to Base-2.js
@@ -1,38 +1,43 @@
 function binary(decimal) {
-	let bin
-	if(decimal <= 0)
-		{
-			return 0;
-		}
-	else if(decimal < 1024)
-		{
-			let arr = [1,2,4,8,16,32,64,128,256,512]
-			let i = 0;
-			while(decimal > arr[i])
-				{
-					i++;
-				}
-			let total = arr[--i];
-			bin = 1;
-			while(i > 0)
-				{
-                    i--;
-					if(decimal >= (total + arr[i]))
-						{
-							total += arr[i];
-							bin = (bin*10)+1
-						}
-					else{
-						bin = (bin*10);
-					}
-				}
-		}
-        else
-        {
-            alert("Number Should Be Less Than 1024");
-            return "Number Should Be Less Than 1024"
-        }
-	return bin;
+
+	/* Normal Way To Do */
+
+	// let bin
+	// if(decimal <= 0)
+	// 	{
+	// 		return 0;
+	// 	}
+	// else if(decimal < 1024)
+	// 	{
+	// 		let arr = [1,2,4,8,16,32,64,128,256,512]
+	// 		let i = 0;
+	// 		while(decimal > arr[i])
+	// 			{
+	// 				i++;
+	// 			}
+	// 		let total = arr[--i];
+	// 		bin = 1;
+	// 		while(i > 0)
+	// 			{
+    //                 i--;
+	// 				if(decimal >= (total + arr[i]))
+	// 					{
+	// 						total += arr[i];
+	// 						bin = (bin*10)+1
+	// 					}
+	// 				else{
+	// 					bin = (bin*10);
+	// 				}
+	// 			}
+	// 	}
+    //     else
+    //     {
+    //         alert("Number Should Be Less Than 1024");
+    //         return "Number Should Be Less Than 1024"
+    //     }
+
+	/* JS Way To Do It :) */
+	return parseInt(decimal).toString(2);
 }
 let num = prompt("Enter A Number To Get Binary Conversion (Less Than 1024)");
 document.write(binary(num));
